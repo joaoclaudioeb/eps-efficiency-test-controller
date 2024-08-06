4db25a751d0a4679242fbf5f87a6c0e081dcd733
joaoclaudioeb <joaoclaudiobarcellos@gmail.com>
2024-08-06 13:23:22 -0300

    feat: add the preliminary PWM driver

--- drivers/pwm_module.cpp
+++ drivers/pwm_module.cpp
@@ -1,10 +1,37 @@
 /**
  * @file pwm_module.cpp
  *
- * @brief This file contains the source code defining the class for the PWM module.
+ * @brief TODO.
+ *
+ * @details TODO. 
  *
  * @author João Cláudio Elsen Barcellos <joao.barcellos@posgrad.ufsc.br>
  * @version 0.0.0
- *
  * @date 29/07/2024
  */
+
+/* Abstraction-related headers */
+#include "pwm_module.h"
+
+PWM::PWM() {}
+PWM::~PWM() {}
+
+void PWM::configPwmPin(pwm_config_t config) {
+    gpio_set_function(static_cast<unsigned int>(config.pin), static_cast<gpio_function>(gpio_func::PWM));
+}
+
+void PWM::enablePwmPin(pwm_config_t config, pwm_state state) {
+    unsigned int slice = pwm_gpio_to_slice_num(static_cast<unsigned int>(config.pin)); 
+    pwm_set_enabled(slice, static_cast<bool>(state));
+}
+
+void PWM::setDutyCycle(pwm_config_t config) {
+    unsigned int slice = pwm_gpio_to_slice_num(static_cast<unsigned int>(config.pin)); 
+    unsigned int channel = pwm_gpio_to_channel(static_cast<unsigned int>(config.pin));
+
+    unsigned short int wrap = (125000000/config.frequency) -1;
+    unsigned short int level = (wrap*config.duty_cycle)/100;
+
+    pwm_set_wrap(slice, wrap);
+    pwm_set_chan_level(slice, channel, level);
+}
