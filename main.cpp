106c9a83b0e8f624734381e4d5162ce4b5a0c687
Ramon de Araujo Borba <ramonborba97@gmail.com>
2024-08-06 16:51:25 -0300

    feat: add basic test routine task

--- main.cpp
+++ main.cpp
@@ -11,10 +11,13 @@
  */
 
 // TODO: Add headers here
+#include "tasks/basic_test_routine.h"
 
 int main(void)
 {
     // TODO: Add some code here
+    xTaskCreate(basicTestRoutineTask, TASK_BASIC_TEST_ROUTINE_TASK_NAME, TASK_BASIC_TEST_ROUTINE_TASK_STACK_SIZE, nullptr, TASK_BASIC_TEST_ROUTINE_TASK_PRIORITY, nullptr);
+    vTaskStartScheduler();
     return 0;
 }
 
